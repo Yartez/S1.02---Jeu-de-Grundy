7801010a8b3ef6f232e30da71ec3976d1d2cae62
Audebert Giovani <115887411+Yartez@users.noreply.github.com>
2024-12-18 08:33:09 +0100

    Add files via upload

---
+++ src/GrundyRecBruteEff.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * Jeu de Grundy avec IA pour la machine
+ * Ce programme ne contient que les méthodes permettant de tester jouerGagnant()
+ * Cette version est brute sans aucune amélioration
+ *
+ * @author G.Audebert, Y.Monnot
+ */
+
+class GrundyRecBrute {
+
+    /**
+     * Variables globales
+     */
+    long cpt; // compteur d'opérations élémentaires
+    /**
+     * Constructeur de la classe
+     */
+    GrundyRecBrute monGrundy = new GrundyRecBrute(); // Pour les méthodes de la classe GrundyRecBrute
+
+    /**
+     * Méthode principal du programme
+     */
+    void principal() {
+        
+    }
+
+    /**
+     * Boucle principale du jeu
+     */
+    void boucleJeu() {
+
+    }
+}
